2ab402b05c5e264267d57c74dfd6341958363851
Sasha Levshin <ocarin31@yandex.ru>
2021-03-01 12:50:53 +0700

    Fix config usage

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -19,8 +19,9 @@ func main() {
 	h := handler.NewHandler()
 	r := chi.NewRouter()
 	r.Get("/hello", h.Hello)
-	log.Print("starting server")
-	err = http.ListenAndServe(":8000", r)
+	path := cfg.Host+":"+cfg.Port
+	log.Printf("starting server at %s", path)
+	err = http.ListenAndServe(path, r)
 	log.Fatal(err)
 	log.Print("shutting server down")
 }
