35d204ef0b9a1e8490937de226221b0289912b08
Sasha Levshin <ocarin31@yandex.ru>
2021-03-01 12:24:47 +0700

    Add config parsing

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"net/http"
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	h := handler.NewHandler()
 	r := chi.NewRouter()
 	r.Get("/hello", h.Hello)
 	log.Print("starting server")
-	err := http.ListenAndServe(":8000", r)
+	err = http.ListenAndServe(":8000", r)
 	log.Fatal(err)
 	log.Print("shutting server down")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module workshop
 
 go 1.16
 
-require github.com/go-chi/chi v1.5.3 // indirect
+require (
+	github.com/go-chi/chi v1.5.3 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.2.5 // indirect
+)
